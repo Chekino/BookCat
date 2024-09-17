7fc648294e0281f18916bede29a1a820d85d41dd
Cheikh <diakite.cheikh200@gmail.com>
2024-09-17 22:53:17 +0200

    image deploy

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"], // Permet uniquement les ressources du même domaine
-      imgSrc: ["'self'", "http://localhost:5000"], // Autorise les images provenant de ton serveur backend
+      imgSrc: ["'self'", "https://bookcat.vercel.app"], // Autorise les images provenant de ton serveur backend
       // Ajoute d'autres règles pour les scripts, styles si nécessaire
     },
   })
