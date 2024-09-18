f8cddaafc51963c785001e557b2b0ad44e286671
Cheikh <diakite.cheikh200@gmail.com>
2024-09-18 16:23:48 +0200

    BookCat v-1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ce middleware redirige toutes les routes vers index.html pour le routage côté client
+  build: {
+    outDir: "dist",
+  },
 });
